3cac2257959546a701459732a8750e56d723f9f8
kidlatpogi <dzeref4000@gmail.com>
2025-12-10 00:42:57 +0800

    Remove unused migration and documentation files

--- db/add_admin_account.sql
+++
@@ -1,5 +0,0 @@
--- Add admin account with email admin@mypc.com
--- Password: admin123
-
-INSERT IGNORE INTO `users` (`email`, `password_hash`, `first_name`, `last_name`, `role`, `is_admin`) VALUES
-('admin@mypc.com', '$2y$10$9ogJGajWf62VDgDyGm4i4e1r2lx0.UeI4wxff7u7ROnLlFTPbHgSS', 'Admin', '', 'admin', 1);
