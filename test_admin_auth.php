f38e840354aa58c8d03bfffb24299fba418133d9
kidlatpogi <dzeref4000@gmail.com>
2026-01-20 14:56:42 +0800

    chore: Remove temporary test files

--- test_admin_auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_POST = array('action' => 'adminLogin', 'email' => 'superadmin@mypc.com', 'password' => '@dmin123');
-$_SERVER['REQUEST_METHOD'] = 'POST';
-$_SERVER['HTTP_ORIGIN'] = 'http://localhost';
-$_SERVER['REQUEST_URI'] = '/HTML_PHP/admin_auth.php';
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-include 'HTML_PHP/admin_auth.php';
-?>
